556ea7c484b4087b90e668d02bff566451615611
J1286 <twelve.zero@gmail.com>
2026-07-22 10:55:36 -0700

    Update config.js

--- config.js
+++ config.js
@@ -14,23 +14,23 @@ const PARSER_PLUGINS = {};
   PARSER_PLUGINS.redline360 = {
     parse: parseRedlineWrapper,
     confidence: 0.95
-  },
+  };
   PARSER_PLUGINS.aag = {
     parse: parseAAGWrapper,
     confidence: 0.95
-  },
+  };
   PARSER_PLUGINS.tdot = {
     parse: parseTDOTWrapper,
     confidence: 0.9
-  },
+  };
   PARSER_PLUGINS.z1 = {
     parse: parseZ1Wrapper,
     confidence: 0.9
-  },
+  };
   PARSER_PLUGINS.newdealer = {
     parse: parseNewDealerWrapper,
     confidence: 0.9
-  },
+  };
   PARSER_PLUGINS.generic = {
     parse: parseGeneric,
     confidence: 0.5
