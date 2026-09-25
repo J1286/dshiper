ddb5d57c5ba83b9dc22f96e914a72831db422f8f
J1286 <twelve.zero@gmail.com>
2026-09-25 09:30:24 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -589,6 +589,10 @@ function testSKURegression() {
       input: "LHP-MST10BK-V2-TM",
       expected: "LHP-MST10BK-V2-TM"
     },
+	{
+  	  input: "BKT-L-MST79",
+      expected: "BKT-L-MST79"
+    },
     {
       input: "SPE-RMX-F15007F3H-P-FS",
       expected: "RMX-F15007F3H-P-FS"
